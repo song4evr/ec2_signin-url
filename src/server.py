36dc39b7ccc8c897e91a6afb4dc77e94832f9b4e
Song Jin <song881014@gmail.com>
2021-01-27 06:27:13 +0000

    add SSRF demo

--- src/server.py
+++ src/server.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import requests 
+from urllib.parse import unquote
 from get_login_url import get_login_url
+
+"""
+
+Caution:
+
+This code is to demonstrate Server-Side Request Forgery, 
+it would leake the ec2 credential to the public.
+
+Do NOT run this code in any ec2 instance without understanding IMDS. 
+
+"""
 
 server = Flask(__name__)
 
-@server.route("/")
-def index():
+@server.route("/url")
+def login_url():
     return get_login_url()
 
+
+@server.route("/", methods = ["GET", "POST"])
+def proxy():
+    if request.method == "POST":
+        request_url = request.form.get('url')
+        r = requests.get(request_url)
+        return render_template("form.html", url=request_url,  contents=r.text) 
+    
+    return render_template("form.html") 
+
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+   server.run(host='localhost', port=8080)
 
